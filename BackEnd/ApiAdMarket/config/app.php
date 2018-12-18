977b0d467756b6177ff87904aece5f6ddd2d9d42
Louis Castel <louis.castel@accopilot.com>
2018-12-18 16:12:02 +0100

    JWT laravel api login register

--- BackEnd/ApiAdMarket/config/app.php
+++ BackEnd/ApiAdMarket/config/app.php
@@ -175,6 +175,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Custom Package
+
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+
+
     ],
 
     /*
@@ -223,6 +228,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class, 
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
 
     ],
 
